c09fef822899b4b047b3902422198071964a7ab3
Nikhil <kujurnikhil0007@gmail.com>
2026-02-10 01:32:22 -0800

    help

---
+++ java.java
@@ -0,0 +1,11 @@
+public  class java{
+public static void main(String[] args) {
+  System.out.println("Hello  world");   
+  System.err.println("Hello world");
+}
+
+static void mode()
+{
+    
+}
+}
---
+++ java2.java
@@ -0,0 +1,8 @@
+
+public class java2 {
+      public static void main(String[] args) {
+         java j = new java();     
+          j.mode();
+        }
+
+}
